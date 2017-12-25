34bbbcc80e8684c285149c44aac23e33cd0eadb3
诺斯 <zengzhi.fzz@alibaba-inc.com>
2017-12-25 16:50:50 +0800

    Add some example shaders from shader toy

--- ShaderToyGL/main.cpp
+++ ShaderToyGL/main.cpp
@@ -11,8 +11,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1200;
+const unsigned int SCR_HEIGHT = 900;
 
 float cursorX = 0;
 float cursorY = 0;
@@ -215,6 +215,9 @@ void processInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS)
+		cout << "MOUSE DOWN" << endl;
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
